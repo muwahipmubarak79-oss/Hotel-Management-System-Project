4f39a25d3a0e8f3e12c1a7458a604ddb3a7e1191
saabrinyy <saabrinabshir44@gmail.com>
2026-05-05 13:18:23 +0300

    Add files via upload

--- admin.py
+++ admin.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Room
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-@admin.register(Room)
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ('room_number', 'room_type', 'price_per_night', 'is_available')
-    list_filter = ('room_type', 'is_available')
-    search_fields = ('room_number',)
+admin.site.register(User, UserAdmin)
