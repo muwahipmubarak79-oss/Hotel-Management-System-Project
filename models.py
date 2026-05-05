378e60be661680089ea855eec147caef422f6970
Radwa <winywalter111@gmail.com>
2026-05-04 22:10:56 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -2,22 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Room(models.Model):
-    # Noocyada qolalka
-    ROOM_TYPES = (
-        ('single', 'Single'),
-        ('double', 'Double'),
-        ('suite', 'Suite'),
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('guest', 'Guest'),
+        ('manager', 'Maneger'),
     )
-    
-    room_number = models.CharField(max_length=10, unique=True)
-    room_type = models.CharField(max_length=10, choices=ROOM_TYPES)
-    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
-    is_available = models.BooleanField(default=True)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return f"Room {self.room_number} - {self.room_type}"
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
 
